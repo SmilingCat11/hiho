7e69c189fb92511a7adfe3c00789bac1796b546b
xingyunbo <wangbo_zju@126.com>
2017-03-05 10:17:10 +0800

    hiho1223 AC
    X 不一定为整数，枚举X时出错，因此对num[i]作右移处理

--- hiho1223.cpp
+++ hiho1223.cpp
@@ -1,5 +1,6 @@
 // hiho1223.cpp -- 不等式
 // WA
+// X 不一定为整数，枚举X时出错，因此对num[i]作右移处理 
 #include <stdio.h> 
 #include <string.h>
 #define maxn 60
@@ -14,19 +15,20 @@ int main()
 	int num[maxn];
 	for (int i = 0; i < n; i++) {
 		scanf("%s%s%d", c, op[i], &num[i]);
-		printf("%s %s %d\n", c, op[i], num[i]);
+		num[i] <<= 1;
+//		printf("%s %s %d\n", c, op[i], num[i]);
 	}
 	
 	int best = 0;
-	for (int x = -10; x < 1010; x++) {
+	for (int x = -10; x < 2010; x++) {
 		int cnt = 0;
 		for (int i = 0; i < n; i++) {
-			if(!(strcmp(op[i], "<")) && x < num[i]) { cnt++;  }
+			if(!(strcmp(op[i], "<")) && x < num[i]) cnt++;
 			if(!(strcmp(op[i], "<=")) && x <= num[i]) cnt++;
 			if(!(strcmp(op[i], "=")) && x == num[i]) cnt++;
 			if(!(strcmp(op[i], ">")) && x > num[i]) cnt++;
 			if(!(strcmp(op[i], ">=")) && x >= num[i]) cnt++;
-			printf("(x = %d) (i = %d) cnt = %d best = %d\n", x, i, cnt, best);
+//			printf("(x = %d) (i = %d) cnt = %d best = %d\n", x, i, cnt, best);
 		}
 		if (cnt > best) best = cnt;
 	}
